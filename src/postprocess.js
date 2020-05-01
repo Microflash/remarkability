2171ddc5edd184b053ae29c5930aeb2ce1adcecb
Microflash <naiyer.app@gmail.com>
2020-05-01 16:28:41 +0530

    chore(docs): cleanup docs generation

--- src/postprocess.js
+++
@@ -1,6 +0,0 @@
-const shell = require('shelljs')
-
-shell.cp('src/content/template.html', 'docs/index.html')
-shell.sed('-i', /<!-- markdown -->/, shell.cat('.sitecache/generated.html'), 'docs/index.html')
-shell.cp('.sitecache/index.min.css', 'docs/index.min.css')
-shell.cp('src/assets/favicon.ico', 'docs/favicon.ico')
